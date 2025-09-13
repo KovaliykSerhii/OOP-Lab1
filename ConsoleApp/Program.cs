817949401fe17d1229dc96cf2699f29b6106024b
KovaliykSerhii <kovaliykserhii@gmail.com>
2025-09-13 16:29:51 +0300

    Calculation of the sum of numbers and output

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 10;
+            Console.WriteLine($"5 + 10 = {x + y}");
+            Console.ReadKey();
+        }
+    }
+}
