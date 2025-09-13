f8d6c58fa5b1ac0528fb74bad0d6920529328a62
KovaliykSerhii <kovaliykserhii@gmail.com>
2025-09-13 17:21:50 +0300

    Add Form1 with button showing message on click

--- WinFormsApp/Form1.cs
+++ WinFormsApp/Form1.cs
@@ -1,9 +1,36 @@
-namespace WinFormsApp;
+using System;
+using System.Windows.Forms;
 
-public partial class Form1 : Form
+namespace WinFormsApp
 {
-    public Form1()
+    public partial class Form1 : Form
     {
-        InitializeComponent();
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCustomComponents();
+        }
+
+        
+        private void InitializeCustomComponents()
+        {
+            
+            Button btn = new Button();
+            btn.Text = "Натисни мене";
+            btn.Location = new System.Drawing.Point(50, 50); 
+            btn.Size = new System.Drawing.Size(120, 40);     
+
+            
+            btn.Click += Btn_Click;
+
+            
+            this.Controls.Add(btn);
+        }
+
+        
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Привіт! Кнопка натиснута.");
+        }
     }
 }
